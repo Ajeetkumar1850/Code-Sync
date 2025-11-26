2af6693f8d11727768f0abe6becfc447708ee576
Ajeetkumar1850 <ajeetkumarssm9987.com>
2025-11-26 21:34:54 +0530

    chore: initial commit

---
+++ client/src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import modal from './modal';
+
+function App() {
+    const [show , setShow] = React.useState(false);
+    return (
+        <div>
+            <h2>React portal example</h2>
+
+            <button onClick={() => setShow(true)}>open Modal</button>
+            <modal>
+            {show ({
+                title: 'My Modal',
+                content: 'This is the content of the modal.',
+                onClose: () => setShow(false)
+            })}
+            </modal>
+        </div>
+    );
+}
